650daebc3b0c69c17c42066889f69f7a8c5b6887
Alex <salispiligrim@yandex.ru>
2025-12-05 16:27:36 +0500

    day_1 добавление account_id в данные таблиц

---
+++ database/migrations/2025_12_05_105941_update_sales_table.php
@@ -0,0 +1,27 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->unique(['account_id', 'date'], 'orders_account_date_unique');
+            $table->foreign('account_id')
+                ->references('id')
+                ->on('accounts')
+                ->onDelete('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('sales', function (Blueprint $table) {
+            $table->dropForeign(['account_id']);
+            $table->dropUnique('orders_account_date_unique');
+            $table->dropColumn('account_id');
+        });
+    }
+};
