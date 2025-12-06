cf70d9c038a010356303e07ce4442c9d85dd8cfa
Alex <salispiligrim@yandex.ru>
2025-12-06 16:17:14 +0500

    day_2 добавил автообновление данных дважды в день

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::exec('php ' . base_path('app/scheduleScripts/autoImportDate.php'))
+    ->twiceDaily(3, 12)
+    ->appendOutputTo(storage_path('logs/importSchedule.log'));
